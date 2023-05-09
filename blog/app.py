642c48a7b4a7029dec4502bff795f2f47f5de4ef
KonstantinMoseyko <90209282+KonstantinMoseyko@users.noreply.github.com>
2023-05-09 13:34:11 +0300

    NEW | lesson_3 User models auth (#3)

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.views.auth import login_manager, auth_app
+from blog.models.database import db
+from blog import commands
 
 
 app = Flask(__name__)
@@ -12,3 +15,13 @@ def index():
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.register_blueprint(auth_app, url_prefix="/auth")
+
+app.cli.add_command(commands.init_db)
+app.cli.add_command(commands.create_users)
+
+app.config["SECRET_KEY"] = "abcdefg123456"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+login_manager.init_app(app)
